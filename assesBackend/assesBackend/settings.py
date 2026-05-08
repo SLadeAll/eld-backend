c6eec02f73fd42c4f0cfa35a86ab601d9f2f505b
Oscar Vera <oscar.jav.vera@gmail.com>
2026-05-08 17:24:48 -0600

    Use dj-database-url to correctly parse DATABASE_URL including sslmode

--- assesBackend/assesBackend/settings.py
+++ assesBackend/assesBackend/settings.py
@@ -77,18 +77,8 @@ WSGI_APPLICATION = 'assesBackend.wsgi.application'
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
 if DATABASE_URL:
-    import urllib.parse
-    url = urllib.parse.urlparse(DATABASE_URL)
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': url.path[1:],
-            'USER': url.username,
-            'PASSWORD': url.password,
-            'HOST': url.hostname,
-            'PORT': url.port or 5432,
-        }
-    }
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)}
 else:
     DATABASES = {
         'default': {
